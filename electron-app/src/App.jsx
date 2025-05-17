a6a996b3f0b0e04b5d1c6f96b3fd2ab474a7a020
Taruna-M <t.mulugur@gmail.com>
2025-05-17 17:07:54 +0530

    feat: setup electron-app

---
+++ electron-app/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1 className="text-xl font-bold">Echooo</h1>
+      {/* You’ll mount CodeEditor and P2PPanel here later */}
+    </div>
+  );
+}
---
+++ electron-app/webpack.renderer.config.js
@@ -0,0 +1,28 @@
+const rules = require('./webpack.rules');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+};
